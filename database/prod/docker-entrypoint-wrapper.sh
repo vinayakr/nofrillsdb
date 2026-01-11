0d7fe13a985d2bea81ee90623e72b5c8346cf68b
Vinayak Rao <vrao1981@gmail.com>
2026-01-10 23:33:13 -0800

    Update docker

---
+++ database/prod/docker-entrypoint-wrapper.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+set -e
+
+# Copy certs from read-only bind mount into writable volume
+mkdir -p /certs
+cp -f /certs-src/server.crt /certs/server.crt
+cp -f /certs-src/server.key /certs/server.key
+cp -f /certs-src/clients_ca.crt /certs/clients_ca.crt
+
+# Fix perms required by Postgres
+chown -R postgres:postgres /certs
+chmod 600 /certs/server.key
+chmod 644 /certs/server.crt /certs/clients_ca.crt
+
+# Hand off to the official entrypoint (it will drop privileges properly)
+exec /usr/local/bin/docker-entrypoint.sh "$@"
