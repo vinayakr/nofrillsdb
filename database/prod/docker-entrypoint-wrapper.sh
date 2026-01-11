54bc4d97d9bf14bbcc807cbb67ae4e119ce9a155
Vinayak Rao <vrao1981@gmail.com>
2026-01-10 23:47:02 -0800

    Update docker

--- database/prod/docker-entrypoint-wrapper.sh
+++ database/prod/docker-entrypoint-wrapper.sh
@@ -6,11 +6,14 @@ mkdir -p /certs
 cp -f /certs-src/server.crt /certs/server.crt
 cp -f /certs-src/server.key /certs/server.key
 cp -f /certs-src/clients_ca.crt /certs/clients_ca.crt
+cp -f /certs-src/pgbouncer.crt /certs/pgbouncer.crt
+cp -f /certs-src/pgbouncer.key /certs/pgbouncer.key
 
 # Fix perms required by Postgres
 chown -R postgres:postgres /certs
 chmod 600 /certs/server.key
 chmod 644 /certs/server.crt /certs/clients_ca.crt
+chmod 644 /certs/pgbouncer.key
 
 # Hand off to the official entrypoint (it will drop privileges properly)
 exec /usr/local/bin/docker-entrypoint.sh "$@"
