2980523fe03c91f20566ac958e37d3f7fa908c35
Vinayak Rao <vrao1981@gmail.com>
2026-01-11 19:40:28 -0800

    Fix database config

--- backend/src/main/kotlin/com/nofrillsdb/config/DatabaseConfig.kt
+++ backend/src/main/kotlin/com/nofrillsdb/config/DatabaseConfig.kt
@@ -1,7 +1,10 @@
 package com.nofrillsdb.config
 
 import com.zaxxer.hikari.HikariDataSource
+import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Qualifier
+import org.springframework.boot.ApplicationRunner
+import org.springframework.boot.autoconfigure.condition.ConditionalOnBean
 import org.springframework.boot.context.properties.ConfigurationProperties
 import org.springframework.boot.jdbc.autoconfigure.DataSourceProperties
 import org.springframework.context.annotation.Bean
@@ -14,6 +17,8 @@ import javax.sql.DataSource
 class DatabaseConfig {
 
     // --- MAIN APP DB (used by JPA + Flyway) ---
+
+    private val log = LoggerFactory.getLogger(javaClass)
 
     @Bean
     @Primary
@@ -41,4 +46,17 @@ class DatabaseConfig {
     @Bean
     fun provisionJdbcTemplate(@Qualifier("provisionDataSource") ds: DataSource) =
         JdbcTemplate(ds)
+
+    @Bean
+    @ConditionalOnBean(name = ["provisioningDataSourceProperties"])
+    fun logProvisioningProps(
+        provisioningDataSourceProperties: DataSourceProperties
+    ) = ApplicationRunner {
+        log.warn(
+            "Provisioning DS props: url='{}' username='{}' driver='{}'",
+            provisioningDataSourceProperties.url,
+            provisioningDataSourceProperties.username,
+            provisioningDataSourceProperties.driverClassName
+        )
+    }
 }
