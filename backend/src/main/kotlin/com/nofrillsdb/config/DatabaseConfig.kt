74dbf0cb9fc5ab550755cfd14a48fbb06269519a
Vinayak Rao <vrao1981@gmail.com>
2026-01-11 19:54:34 -0800

    Fix database config

--- backend/src/main/kotlin/com/nofrillsdb/config/DatabaseConfig.kt
+++ backend/src/main/kotlin/com/nofrillsdb/config/DatabaseConfig.kt
@@ -1,10 +1,7 @@
 package com.nofrillsdb.config
 
 import com.zaxxer.hikari.HikariDataSource
-import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Qualifier
-import org.springframework.boot.ApplicationRunner
-import org.springframework.boot.autoconfigure.condition.ConditionalOnBean
 import org.springframework.boot.context.properties.ConfigurationProperties
 import org.springframework.boot.jdbc.autoconfigure.DataSourceProperties
 import org.springframework.context.annotation.Bean
@@ -17,8 +14,6 @@ import javax.sql.DataSource
 class DatabaseConfig {
 
     // --- MAIN APP DB (used by JPA + Flyway) ---
-
-    private val log = LoggerFactory.getLogger(javaClass)
 
     @Bean
     @Primary
@@ -40,18 +35,11 @@ class DatabaseConfig {
     @Bean(name = ["provisionDataSource"])
     fun provisionDataSource(
         @Qualifier("provisioningDataSourceProperties") props: DataSourceProperties
-    ): DataSource {
-        log.warn("Provisioning DS props BEFORE build: url='{}' username='{}' driver='{}'",
-            props.url, props.username, props.driverClassName
-        )
-        require(!props.url.isNullOrBlank()) { "Missing provisioning.datasource.url" }
-        return props.initializeDataSourceBuilder()
-            .type(HikariDataSource::class.java)
-            .build()
-    }
+    ): DataSource =
+        props.initializeDataSourceBuilder().build()
 
     @Bean
     fun provisionJdbcTemplate(@Qualifier("provisionDataSource") ds: DataSource) =
         JdbcTemplate(ds)
-
+    
 }
